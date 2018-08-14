525032142d2cd95355f12ca5b63876cfb952a182
Kent Johnson <kent.johnson@perkinelmer.com>
2018-08-14 15:12:20 -0400

    Add functions count_phenotypes, merge_and_split_cell_seg_data, split_phenotypes

---
+++ R/count_phenotypes.R
@@ -0,0 +1,55 @@
+# Suppress CMD CHECK notes for things that look like global vars
+utils::globalVariables(c(
+  "Slide ID",
+  "Tissue Category",
+  "."
+))
+
+#' Count phenotypes per slide and tissue category
+#'
+#' @param csd Cell seg data to use. This should already have been filtered
+#'   for the slides or fields of interest.
+#' @param phenotypes A named list of phenotype selectors. Results will be
+#'   reported in the same order as phenotypes are listed here.
+#' @param tissue_categories A character vector of tissue category names
+#'   of interest.
+#' @param include_all If true, include a column for All cells (i.e. total)
+#' @return A data frame with columns for Slide ID, Tissue Category, cell
+#'   counts for each requested phenotype, and total cells
+#' @importFrom magrittr %>%
+#' @export
+count_phenotypes = function(csd, phenotypes, tissue_categories, include_all=TRUE) {
+  selections = csd %>%
+    dplyr::select(`Slide ID`, `Tissue Category`, dplyr::starts_with('Phenotype')) %>%
+    dplyr::filter(`Tissue Category` %in% tissue_categories) %>%
+    dplyr::bind_cols(purrr::map_dfc(phenotypes,
+                             function(pheno) phenoptr::select_rows(., pheno))) %>%
+    dplyr::select(-dplyr::starts_with('Phenotype '))
+
+  pheno_names = names(phenotypes)
+  if (include_all) {
+    selections$`Total Cells` = TRUE
+    pheno_names = c(pheno_names, 'Total Cells')
+  }
+
+  fill=rep(0, length(pheno_names)) %>% as.list %>% rlang::set_names(pheno_names)
+
+  selections = selections %>% dplyr::group_by(`Slide ID`, `Tissue Category`) %>%
+    dplyr::summarize_all(sum) %>%
+    dplyr::ungroup() %>%
+    tidyr::complete(`Slide ID`, `Tissue Category`, fill=fill)
+
+  # Compute totals per phenotype if there are multiple tissue categories
+  if (length(tissue_categories) > 1) {
+    totals = selections %>% dplyr::group_by(`Slide ID`) %>%
+      dplyr::summarize_at(dplyr::vars(-(1:2)), sum)
+    totals$`Tissue Category` = 'Total'
+    result = dplyr::bind_rows(selections, totals)
+  } else result = selections
+
+  # Lookup table for ordering tissue categories
+  tissue_order = 1:(length(tissue_categories)+1) %>%
+    rlang::set_names(c(tissue_categories, 'Total'))
+   result %>%
+    dplyr::arrange(`Slide ID`, tissue_order[`Tissue Category`])
+}
---
+++ R/split_phenotypes.R
@@ -0,0 +1,74 @@
+# Functions to merge 'classic' cell seg data files and convert to
+# a phenotype per marker form.
+
+# Suppress CMD CHECK notes for things that look like global vars
+utils::globalVariables(c(
+  "Cell ID",
+  "Sample Name",
+  "Phenotype",
+  "Confidence"
+))
+
+#' Merge several cell seg data files, each with their own `Phenotype` column,
+#' into a single file with separate columns for each phenotype.
+#'
+#' @param files A list or vector of file paths
+#' @return A single data frame containing merged data and columns for each
+#'   phenotype.
+#' @importFrom magrittr %>%
+#' @export
+merge_and_split_cell_seg_data = function(files) {
+  files = unlist(files)
+  if (!is.character(files) || !all(purrr::map_lgl(files, file.exists)))
+    stop('Please pass a list of paths to existing cell seg data files.')
+
+  csd = phenoptr::read_cell_seg_data(files[1]) %>% split_phenotypes()
+
+  for (file in files[-1]) {
+    csd2 = phenoptr::read_cell_seg_data(file) %>%
+      split_phenotypes()%>%
+      dplyr::select(`Sample Name`, `Cell ID`, dplyr::starts_with('Phenotype '))
+    if (nrow(csd2) != nrow(csd))
+      stop(paste0('Number of rows in "', files[1], '" does not match "', file, '".'))
+    csd = dplyr::left_join(csd, csd2 )
+  }
+
+  csd
+}
+
+#' Split a phenotype column containing multiple positivities
+#' into multiple columns, one for each single phenotype.
+#'
+#' @param csd Cell seg data to use.
+#' @return A new data frame with the `Phenotype` column replaced with
+#'   individual columns per phenotype and the `Confidence` column removed.
+#' @importFrom magrittr %>%
+#' @export
+split_phenotypes = function(csd) {
+  if (!'Phenotype' %in% names(csd))
+    stop('Cell seg data does not have a Phenotype column.')
+
+  # Look for positive phenotypes
+  phenotypes = unique(csd$Phenotype) %>%
+    stringr::str_split('/') %>%
+    purrr::flatten_chr()
+  positives = phenotypes[endsWith(phenotypes, '+')] %>% unique()
+
+  if (length(positives) == 0)
+    stop('No positive phenotypes found.')
+
+  # Make a new column for each positive phenotype
+  new_columns = purrr::map(positives, ~{
+    positive = .x
+    negative = stringr::str_replace(positive, '\\+$', '-')
+    result = rep(negative, nrow(csd))
+    result[stringr::str_detect(csd$Phenotype, stringr::fixed(positive))] = positive
+    result
+  })
+
+  new_names = paste('Phenotype', stringr::str_remove(positives, '\\+$'))
+  new_columns = new_columns %>% rlang::set_names(new_names)
+
+  csd %>% dplyr::select(-Phenotype, -Confidence) %>%
+    dplyr::bind_cols(new_columns)
+}
