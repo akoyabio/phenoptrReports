d67edfa3163e83b7a16858f5a76cf7329113fd2b
Kent Johnson <kent.johnson@perkinelmer.com>
2018-08-17 11:12:29 -0400

    A little nicer format for Excel output.

--- R/excel_helpers.R
+++ R/excel_helpers.R
@@ -2,7 +2,7 @@
 
 pct_style = openxlsx::createStyle(numFmt='0%')
 bold_style = openxlsx::createStyle(textDecoration='bold',
-                                   halign='center')
+                                   halign='center', wrapText=TRUE)
 
 #' Write a counts table to an Excel workbook
 #'
@@ -53,6 +53,10 @@ write_expression_sheet = function(wb, exprs,
     dplyr::select(-dplyr::contains('Count'))
 
   write_sheet(wb, exprs, sheet_name, sheet_title, 3)
+
+  # Make the expression columns a little wider
+  openxlsx::setColWidths(wb, sheet_name, 3:ncol(exprs), 14)
+
 }
 
 write_sheet <- function(wb, d, sheet_name, sheet_title, header_col) {
@@ -60,12 +64,21 @@ write_sheet <- function(wb, d, sheet_name, sheet_title, header_col) {
   openxlsx::addWorksheet(wb, sheet_name)
 
   # Write a bold header
-  openxlsx::writeData(wb, sheet_name, startCol=header_col,
-                      data.frame(title=sheet_title), colNames=FALSE)
-  openxlsx::addStyle(wb, sheet_name, bold_style, rows=1, cols=header_col)
+  openxlsx::writeData(wb, sheet_name, startCol=header_col, sheet_title)
+  openxlsx::addStyle(wb, sheet_name, bold_style, rows=1, cols=1:header_col)
   openxlsx::mergeCells(wb, sheet_name, rows=1, cols=header_col:ncol(d))
 
   # Write the table
   openxlsx::writeData(wb, sheet_name, d, startRow=2, headerStyle=bold_style)
+
+  # Make the initial headers be two rows
+  for (col in 1:(header_col-1)) {
+    openxlsx::writeData(wb, sheet_name, startCol=col,
+                        data.frame(xx=names(d)[col]), colNames=FALSE)
+    openxlsx::mergeCells(wb, sheet_name, rows=1:2, cols=col)
+  }
+
+  # Wider first column
+  openxlsx::setColWidths(wb, sheet_name, 1, 'auto')
 }
 
