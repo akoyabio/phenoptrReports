2fc2ccf8260d0f6fe8d35ae3e9612540b77a9c96
Kent Johnson <kent.johnson@perkinelmer.com>
2018-09-04 17:47:04 -0400

    Fix rounding in H-Score

--- R/positivity.R
+++ R/positivity.R
@@ -146,7 +146,7 @@ compute_h_score = function(csd, measure, tissue_categories,
       `1+` = round(`Count of 1+`/Total, 3),
       `2+` = round(`Count of 2+`/Total, 3),
       `3+` = round(`Count of 3+`/Total, 3),
-      `H-Score` = as.integer(100 * (`1+` + 2*`2+` + 3*`3+`))
+      `H-Score` = round(100 * (`1+` + 2*`2+` + 3*`3+`))
     )
   d
 }
