686ed7967be925a51c1a93e88486a4781be1c7fe
Kent Johnson <kent.johnson@perkinelmer.com>
2018-10-16 16:32:11 -0400

    Fix Results.xlsx to match modified H-Score thresholds. Comment out some dev code that should not be checked in .

--- inst/analysis_app/global.R
+++ inst/analysis_app/global.R
@@ -5,7 +5,7 @@ library(phenoptrReports)
 library(shiny)
 
 base_dir = system.file('analysis_app', package='phenoptrReports') # For release
-base_dir = getwd() # For development
+# base_dir = getwd() # For development
 source(file.path(base_dir, 'phenotype_module.R'))
 source(file.path(base_dir, 'files_module.R'))
 source(file.path(base_dir, 'formatters.R'))
