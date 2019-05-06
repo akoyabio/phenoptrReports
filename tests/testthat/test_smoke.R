akoyabio/phenoptrReports

commit b0fab0b5d22f210a2271b0febffc4edf27d643a9

Change deprecated `data_frame` to `tibble`.

R/component_level_helpers.R
@@ -37,7 +37,7 @@ component_ridge_plot = function(path, quantiles=NULL) {
   quants = comps %>%
     tidyr::nest(-Fluor, -Source) %>%
     dplyr::mutate(q=purrr::map(data, ~quantile(.$value, quantiles)
-           %>% as.list() %>% tibble::as_data_frame())) %>%
+           %>% as.list() %>% tibble::tibble())) %>%
     dplyr::select(-data) %>% tidyr::unnest()
 
   # For plotting a long data frame is better

inst/rmd/Unmixing_quality_report.Rmd
@@ -251,11 +251,11 @@ density_fig_height = 0.5 * length(comp_files)
 
 ```{r fig.height=density_fig_height}
 # Make density plots using random sampled data
-all_primaries = data_frame(
+all_primaries = tibble(
   Source=factor(comp_names, levels=comp_names), 
   Fluor=factor(primary_fluors, levels=unique(primary_fluors)),
   data=purrr::map(all_data, 
-                  ~data_frame(primary=as.numeric(.x$primary)) %>%
+                  ~tibble(primary=as.numeric(.x$primary)) %>%
                     sample_frac(size=0.1))) %>% 
   unnest
 

R/mean_expression.R
@@ -122,7 +122,7 @@ compute_mean_expression_many = function(
   result %>% dplyr::group_by(`Slide ID`, name) %>%
     tidyr::nest() %>%
     dplyr::mutate(data=purrr::map(data, function(d) {
-      total = dplyr::data_frame(count=sum(d$count),
+      total = tibble::tibble(count=sum(d$count),
                                 mean=sum(d$count*d$mean)/sum(d$count))
       total$`Tissue Category` = 'All'
       dplyr::bind_rows(d, total)
@@ -196,5 +196,5 @@ compute_mean_expression = function(
   # Report NA rather than NaN
   m[is.nan(m)] = NA
 
-  tibble::data_frame(count=length(d), mean=m)
+  tibble::tibble(count=length(d), mean=m)
 }

R/positivity.R
@@ -79,7 +79,7 @@ compute_positivity = function(csd, phenotype, positivity) {
 
   d = d[phenoptr::select_rows(d, positivity),]
   positive = nrow(d)
-  tibble::data_frame(count=count, positive=positive, fraction=positive/count)
+  tibble::tibble(count=count, positive=positive, fraction=positive/count)
 }
 
 #' Compute H-Score based on parameters in a score data file

commit 2fa151ca57801f3d44bc25c9d57102701330bf0e

Add some much-needed smoke tests.
Fix component levels report (broken by b0fab0b5)

Some tests only run on dev machine for now, to get them going.

R/component_level_helpers.R
@@ -37,7 +37,7 @@ component_ridge_plot = function(path, quantiles=NULL) {
   quants = comps %>%
     tidyr::nest(-Fluor, -Source) %>%
     dplyr::mutate(q=purrr::map(data, ~quantile(.$value, quantiles)
-           %>% as.list() %>% tibble::tibble())) %>%
+           %>% as.list() %>% tibble::as_tibble())) %>%
     dplyr::select(-data) %>% tidyr::unnest()
 
   # For plotting a long data frame is better

R/reports.R
@@ -39,8 +39,11 @@ write_summary_report = function(csd_path=NULL, csd=NULL,
 #'   by [write_counts_sheet], etc.
 #' @param output_path Path to write the resulting file.
 #' @param .by Name of the grouping parameter in the worksheets
+#' @param  max_slides_per_plot Maximum number of slides or samples
+#'  to show on each plot
 #' @export
-write_summary_charts = function(workbook_path, output_path, .by='Slide ID') {
+write_summary_charts = function(workbook_path, output_path,
+                                .by='Slide ID', max_slides_per_plot=20) {
   stopifnot(file.exists(workbook_path))
 
   if (is.null(output_path))
@@ -59,7 +62,8 @@ write_summary_charts = function(workbook_path, output_path, .by='Slide ID') {
   rmarkdown::render(rmd_path, output_file=output_path, quiet=TRUE,
                     intermediates_dir=temp_dir_by(output_path),
                     output_format=output_format,
-                    params=list(workbook_path=workbook_path, .by=.by))
+                    params=list(workbook_path=workbook_path, .by=.by,
+                                max_slides_per_plot=max_slides_per_plot))
 }
 
 #' Create an unmixing quality report for simplex samples

inst/rmd/Component_levels_report.Rmd
@@ -8,8 +8,10 @@ output:
 params:
   export_path: 
     label: "Path to directory with component data"
-    value: D:/ManualVsBond/Test
+    value: C:/Research/phenoptrTestData/component_levels_report_test
     input: file
+editor_options: 
+  chunk_output_type: console
 ---
 
 <style type="text/css">

inst/rmd/Unmixing_quality_report.Rmd
@@ -5,7 +5,7 @@ output: rmarkdown::html_vignette
 params:
   export_path: 
     label: "Path to directory with component data"
-    value: C:\Research\phenoptrTestData\Export_test
+    value: C:\Research\phenoptrTestData\unmixing_quality_report_test
     input: file
 ---
 

man/write_summary_charts.Rd
@@ -4,7 +4,8 @@
 \alias{write_summary_charts}
 \title{Create summary charts from the results of an analysis}
 \usage{
-write_summary_charts(workbook_path, output_path, .by = "Slide ID")
+write_summary_charts(workbook_path, output_path, .by = "Slide ID",
+  max_slides_per_plot = 20)
 }
 \arguments{
 \item{workbook_path}{Path to an Excel file containing sheets written
@@ -13,6 +14,9 @@ by \link{write_counts_sheet}, etc.}
 \item{output_path}{Path to write the resulting file.}
 
 \item{.by}{Name of the grouping parameter in the worksheets}
+
+\item{max_slides_per_plot}{Maximum number of slides or samples
+to show on each plot}
 }
 \description{
 Creates a Microsoft Word file or HTML report containing summary charts

tests/testthat/test_smoke.R
@@ -80,3 +80,43 @@ test_that("file generation works", {
   expected_path = file.path(data_dir, 'Results_by_sample.xlsx')
   test_file_generation(data_dir, output_dir, expected_path, .by='Sample Name')
 })
+
+# Test writing charts with multiple pages per chart
+test_that("Chart segmentation works", {
+  workbook_path = normalizePath(test_path('test_data/Results_by_sample.xlsx'),
+                                winslash='/', mustWork=FALSE)
+  charts_path = normalizePath(
+    test_path('results_by_sample/Charts_segmented.docx'),
+    winslash='/', mustWork=FALSE)
+
+  if (!dir.exists(dirname(charts_path))) dir.create(dirname(charts_path))
+  if (file.exists(charts_path)) file.remove(charts_path)
+  write_summary_charts(workbook_path, charts_path, .by='Sample Name',
+                       max_slides_per_plot=5)
+  expect(file.exists(charts_path))
+})
+
+# Some really basic smoke tests - just make sure the reports run
+# These work on dev machine only for now :-/
+
+test_that("Unmixing quality report runs", {
+  export_path = 'C:/Research/phenoptrTestData/unmixing_quality_report_test'
+  skip_if_not(dir.exists(export_path))
+
+  report_path = file.path(export_path, 'Unmixing_quality_report.html')
+  if (file.exists(report_path)) file.remove(report_path)
+
+  unmixing_quality_report(export_path)
+  expect(file.exists(report_path))
+})
+
+test_that("Component levels report runs", {
+  export_path = 'C:/Research/phenoptrTestData/component_levels_report_test'
+  skip_if_not(dir.exists(export_path))
+
+  report_path = file.path(export_path, 'Component_levels_report.html')
+  if (file.exists(report_path)) file.remove(report_path)
+
+  component_levels_report(export_path)
+  expect(file.exists(report_path))
+})