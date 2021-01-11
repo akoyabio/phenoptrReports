b481d117c459dcc0e2f99f93b9d246a8812910c7
Kent Johnson <kjohnson@akoyabio.com>
2021-01-11 13:21:35 -0500

    Add a test for `split_phenotypes` with multi-schema data
    Update function docs

--- tests/testthat/test_split_phenotypes.R
+++ tests/testthat/test_split_phenotypes.R
@@ -21,3 +21,21 @@ test_that("make_unique_names works", {
                'baz_merge_cd3_data_3', 'baz_merge_cd3_data_4')
   expect_equal(make_unique_names(paths), expected)
 })
+
+test_that('split_phenotypes works with multi-schema data', {
+  csd_path = "C:/Research/phenoptrTestData/Multi-schema/test_multi_schema.txt"
+  skip_if_not(file.exists(csd_path))
+
+  csd = phenoptr::read_cell_seg_data(csd_path, col_select='phenoptrReports')
+
+  # Start with three multi-schema columns
+  expect_equal(sum(stringr::str_detect(names(csd), 'Phenotype')), 3)
+
+  # Splitting creates six columns and removes the Confidence columns
+  csd = split_phenotypes(csd)
+  expect_equal(sum(stringr::str_detect(names(csd), 'Phenotype')), 6)
+  expect_equal(sum(stringr::str_detect(names(csd), 'Confidence')), 0)
+  expected_names = c("Phenotype PDL-1", "Phenotype PD-1", "Phenotype CD68",
+                     "Phenotype CK", "Phenotype CD8", "Phenotype Ki67")
+  expect_equal(expected_names %in% names(csd), rep(TRUE, length(expected_names)))
+})
