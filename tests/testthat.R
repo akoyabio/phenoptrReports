7af1b0178cff44bea518bc52afea926a6bac6d64
Christian Rickert <rc.login@icloud.com>
2023-11-05 08:48:34 -0700

    enable running tests locally

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(phenoptrReports)
+library(rstudioapi)
 
+# set path to current script directory
+setwd(dirname(getSourceEditorContext()$path))
+
+# run test script with test data
 test_check("phenoptrReports")
